66567426633b8462c82faab6815720b53ba5b03a
Sergey Shpak <sergey@softrize.com>
2019-05-28 22:47:20 +0300

    * Updated configs for prod

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,4 @@
-export const baseURL = "http://157.230.135.10:8080"; // TODO add https://api.voxpopgames.com
+export const baseURL = "http://www.voxpopgames.com:8080"; // TODO add https://api.voxpopgames.com
+// export const baseURL = "http://157.230.135.10:8080"; // TODO add https://api.voxpopgames.com
 
 export const authConfig = {};
--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ module.exports = {
   APP_NAME,
   APP_VERSION,
   WINDOW_WEBTORRENT: `file://${path.join(__dirname, "..", "static", "webtorrent.html")}`,
-  TRACKER_ANNOUNCE_LIST: [["ws://157.230.135.10:8000"], ["udp://157.230.135.10:8000"]],
+  // TRACKER_ANNOUNCE_LIST: [["ws://157.230.135.10:8000"], ["udp://157.230.135.10:8000"]],
+  TRACKER_ANNOUNCE_LIST: [["ws://www.voxpopgames.com:8000"], ["udp://www.voxpopgames.com:8000"]],
   PIECE_LENGTH: 1024 * 1024 // 1MB
 };
